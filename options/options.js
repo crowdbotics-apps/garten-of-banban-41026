81d9feb24a72d8e028036096780ad4c1e3587550
Crowdbotics <team@crowdbotics.com>
2023-05-26 21:35:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "garten-of-banban-41026",
+  url: "https://garten-of-banban-41026.botics.co",
+  api: "https://garten-of-banban-41026.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
